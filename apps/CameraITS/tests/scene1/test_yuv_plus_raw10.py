62cdc3aba3b38d410193c42166958d7871fbef9c
Timothy Knight <tknight@google.com>
2014-09-08 17:53:13 -0700

    CameraITS: Fixed bug in yuv_plus_raw10 test
    Change-Id: I5b072909aa3c856533d05051a6950d9a00f24d38

--- apps/CameraITS/tests/scene1/test_yuv_plus_raw10.py
+++ apps/CameraITS/tests/scene1/test_yuv_plus_raw10.py
@@ -37,8 +37,8 @@ def main():
         cap_raw, cap_yuv = cam.do_capture(req,
                 [{"format":"raw10"}, {"format":"yuv"}])
 
-        img = its.image.convert_capture_to_rgb_image(cap_yuv, True)
-        its.image.write_image(img, "%s_yuv.jpg" % (NAME))
+        img = its.image.convert_capture_to_rgb_image(cap_yuv)
+        its.image.write_image(img, "%s_yuv.jpg" % (NAME), True)
         tile = its.image.get_image_patch(img, 0.45, 0.45, 0.1, 0.1)
         rgb0 = its.image.compute_image_means(tile)
 
